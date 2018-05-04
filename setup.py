16ceb709b7f366e8792a584b94c2b7ebaf3c4583
Marion <marion.le-bras@esial.net>
2018-05-03 21:00:37 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='studious-octo-parakeet',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='GNU',
+    author='marion',
+    author_email='',
+    description='studious-octo-parakeet'
+)
